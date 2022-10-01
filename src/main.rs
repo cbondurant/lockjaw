e2860dd7c5065cfb1331ccb62959eb847ab4c1b3
Conner Bondurant <conner.bondurant@triptych.me>
2022-10-01 13:07:17 -0400

    Chapter 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+	println!("Lockjaw Version 0.1.0");
+	println!("Press Ctrl+c to Exit");
+
+	let mut buff = String::new();
+	let stdin = io::stdin();
+
+	loop {
+		print!("lj> ");
+		io::stdout().flush().unwrap();
+
+		if let Ok(_) = stdin.read_line(&mut buff) {
+			println!("No! You're a {}", buff);
+		}
+	}
 }
